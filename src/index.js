47ae4cf08c8ad22f3b37e7904e041e8e4fc50a91
Kamil Dziedzic <kamil@dziedzic.es>
2022-06-24 20:24:57 +0200

    Save PDF.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import "./index.scss"
 import PdfFile from "./Kamil Dziedzic - Software Engineer.pdf"
 import { saveAs } from 'file-saver'
 
-//if (!navigator.webdriver) {
-//    saveAs(PdfFile, PdfFile)
-//}
+if (!navigator.webdriver) {
+   saveAs(PdfFile, PdfFile)
+}
